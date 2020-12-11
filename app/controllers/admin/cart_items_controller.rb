367690ac1e86f8cf6eaf470694a9731c805d03c2
khoinguyenkc <khoinguyenkc@gmail.com>
2020-12-11 15:50:39 -0800

    implemented a Create action in cart items controller

---
+++ app/controllers/admin/cart_items_controller.rb
@@ -0,0 +1,22 @@
+class Admin::CartItemsController < ApplicationController
+    def create
+        #when user add to cart a product, a form is submitted here
+        @cartitem = CartItem.create(cart_item_params)
+        #its OUR job, not user, to ensure this works. 
+        amount = @cartitem.price * @cartitem.quantity
+        @cartitem.amount = amount
+        @cartitem.save
+        
+
+
+    end
+
+    private
+    def cart_item_params
+        params.require(:cart_item).permit(:price, product_id, cart_id, :quantity)
+        #i havent figured out from where will we get the note and discount yet, we'll just getting the basics to work first
+    end
+
+
+    
+end
