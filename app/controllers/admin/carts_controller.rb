f3242a95c039beab36bd7048c282afc227b95322
khoinguyenkc <khoinguyenkc@gmail.com>
2020-12-14 01:08:45 -0800

    implemente elementary view for cart

--- app/controllers/admin/carts_controller.rb
+++ app/controllers/admin/carts_controller.rb
@@ -1,4 +1,4 @@
-class CartsController < ApplicationController
+class Admin::CartsController < ApplicationController
     def show
         #dont need to pass. views will grab cart from current_card helper method
     end
