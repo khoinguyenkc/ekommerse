1ef94943d56c5c1dd70bd50561129c3f7cb05907
khoinguyenkc <khoinguyenkc@gmail.com>
2020-12-14 20:54:20 -0800

    fixed unique products... methods in Cart, and implement Cart view

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,12 @@ class Cart < ApplicationRecord
     has_many :products, through: :cart_items
     belongs_to :order, optional: true
 
+    def unique_products
+        self.products.uniq
+        #this method is perfectly fine. its more user friendly!
+    end
+
+
     def unique_product_tally(product_id)
         #we dont need this. but user wants to see it
         tally = 0
@@ -20,7 +26,7 @@ class Cart < ApplicationRecord
     def unique_product_amount(product_id)
         #@product = Product.find_by(id: product_id)
         amount = 0
-        
+
         self.cart_items.each do | cart_item |
             if cart_item.product_id == product_id
                 #assuming cart_item.amount is already there. otherwise will cause errors
